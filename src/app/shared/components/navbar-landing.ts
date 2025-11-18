095e1f00c192c81d424f40d13ca934fe236712f7
DavidA <dserrudocaina@gmail.com>
2025-11-18 05:41:42 -0500

    Implementado nav

--- src/app/shared/components/navbar-landing.ts
+++ src/app/shared/components/navbar-landing.ts
@@ -8,16 +8,69 @@ import { CommonModule } from '@angular/common';
   template: `
   <div class="navbar">
   <nav class="navegacion-principal">
-    <a href="#"> <img src="/assets/LogoFull.jpg">l</a>
-    <a href="#">Acerca de</a>
-    <a href="#">Servicios</a>
-    <a href="#">Contacto</a>
-
-
+    <a href="#" > <img class="img" src="/assets/LogoFull.jpg"></a>
+    <ul>
+    <li><a href="#">Quienes Somos</a></li>
+    <li><a href="#">Preguntas Frecuentes</a></li>
+    <li><a href="#">Contactenos</a></li>
+    </ul>
+    <a href="#" class="button">Unete Ahora</a>
   </nav>
   </div>
 
   `,
-  styles: []
+  styles: [`
+    .navbar {
+      background-color: #FFFFFF;
+      padding: 1.5rem 0;
+      margin: 0;
+      font-size: 20px;
+      padding-left: 46px;
+      padding-right: 46px;
+    }
+
+    nav{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    ul {
+      display: flex;
+      flex-direction: row;
+    }
+
+    li {
+      margin-left: 6rem;
+      list-style: none;
+      margin-right: 6rem;
+    }
+
+    ul {
+      display: flex;
+      align-items: center;
+    }
+
+    a{
+      color: #000000;
+      text-decoration: none;
+      font-family: "Poppins", sans-serif;
+      font-weight: 500;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+    }
+    .img {
+      height: 38px;
+    }
+
+    .button {
+      background-color: #B88E2F;
+      color: #FFFFFF;
+      padding: 11px 40px;
+      border-radius: 8px;
+    }
+  `
+  ]
 })
 export class NavbarLandingComponent {}
